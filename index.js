8690fd39993802596cff77b006cdc246fdec24c4
ANDRÉ ARAÚJO MENDONÇA <andremendoncamineiro@gmail.com>
2025-11-03 02:03:40 -0300

    projeto node com teste jest

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Pipeline CI/CD funcionando!' });
+});
+
+// Iniciamos o servidor apenas se não estivermos em modo de teste
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+}
+
+// Exportamos o 'app' para que nossos testes possam importá-lo
+module.exports = app;
---
+++ index.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('./index'); 
+
+describe('Testes da API', () => {
+  
+  it('Deve retornar status 200 e a mensagem correta na rota GET /', async () => {
+    const res = await request(app).get('/');
+    
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.message).toBe('Pipeline CI/CD funcionando!');
+  });
+
+});
