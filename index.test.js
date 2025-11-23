e4910a192e59f44ea8e1ea9a197680bf3b6c4bc7
ANDRÉ ARAÚJO MENDONÇA <andremendoncamineiro@gmail.com>
2025-11-23 01:40:14 -0300

    alteracao correta

--- index.test.js
+++ index.test.js
@@ -4,10 +4,10 @@ const app = require('./index');
 describe('Testes da API', () => {
   
   it('Deve retornar status 200 e a mensagem correta na rota GET /', async () => {
-    const res = await request(app).get('/');
+    const res = await request(app).get('/')
     
     expect(res.statusCode).toEqual(200);
-    expect(res.body.message).toBe('Pipeline CI/CD funcionando!');
+    expect(res.body.message).toBe('Pipeline CI/CD funcionando! Teste ao vivo');
   });
 
 });
